fb8b0fe14be3d48dd3660907cbb75b370d85be5c
sangwooyeom <duatkddn@naver.com>
2016-06-28 17:13:37 +0900

    express generator

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res) {
+  res.render('test123', { title: 'test' });
+});
+
+
+module.exports = router;
